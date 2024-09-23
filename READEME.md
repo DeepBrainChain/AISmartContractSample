c70a861e08ee2aa382f7af80a1aec8f9f49b0708
ytbiu <174920143@qq.com>
2024-09-23 18:37:59 +0800

    add doc

--- READEME.md
+++ READEME.md
@@ -1,3 +1,20 @@
 # ERC20 example
 
-you can use Makefile to compile, deploy, upgrade, verify or test the contract.
+
+## fill these dbc testnet config into the [hardhat.config.ts](hardhat.config.ts). Then you can coding and test the contract.
+      rpc_url: 'https://rpc-testnet.dbcwallet.io',
+      api_url: "https://blockscout-testnet.dbcscan.io/api",
+      browser_url: "https://blockscout-testnet.dbcscan.io",
+      chain_id: 5777,
+      network: "dbcTestnet",
+      chainId: 19850818,
+
+
+## you can use Makefile to compile, deploy, upgrade, verify or test the contract.
+
+## these link may useful for developing erc20 contract:
+
+https://wizard.openzeppelin.com/#
+https://docs.openzeppelin.com/contracts/2.x/erc20
+
+
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,53 +15,19 @@ const config: HardhatUserConfig = {
     enabled: true,
   },
   networks: {
-    bitlayertestnet: {
-      url: 'https://testnet-rpc.bitlayer.org', 
-      chainId: 200810,
-      // accounts: ["b5eb18473a135e9edf076c00df53d76575fa86bca530c7a650921161189a4ac"],
-      accounts: [process.env.BITLAYER_PRIVATE_KEY],
-    },
-    bscTestnet :{
-      url: 'https://data-seed-prebsc-1-s3.binance.org:8545',
-      chainId: 97,
-      accounts: [process.env.BSC_PRIVATE_KEY],
-    },
     dbcTestnet: {
       url: 'https://rpc-testnet.dbcwallet.io',
       accounts: [process.env.DBC_TEST_PRIVATE_KEY],
-
-      // accounts: {
-      //   mnemonic: process.env.DBC_TEST_MNEMONIC || '',
-      // },
       chainId: 19850818,
       timeout: 600000,
     },
-    dbcMainnet: {
-      url: 'https://rpc.dbcwallet.io',
-      accounts: {
-        mnemonic: process.env.MNEMONIC || '',
-      },
-      chainId: 19880818,
-      timeout: 600000,
-    }
   },
   etherscan: {
     apiKey: {
       dbcTestnet: 'no-api-key-needed',
       dbcMainnet: 'no-api-key-needed',
-      // An API key needs to be written as the hardhat-verify plugin will require it, and the verification will fail if it is not provided.
-      // The current bitlayer browser has not yet enabled API key verification, so you can write any random string for now.
-      bitlayertestnet: "1234",
     },
     customChains: [
-      {
-        network: "bitlayertestnet",
-        chainId: 200810,
-        urls: {
-          apiURL: "https://api-testnet.btrscan.com/scan/api",
-          browserURL: "https://testnet.btrscan.com/"
-        }
-      },
       {
         network: "dbcTestnet",
         chainId: 19850818,
@@ -70,14 +36,6 @@ const config: HardhatUserConfig = {
           browserURL: "https://blockscout-testnet.dbcscan.io",
         },
       },
-      {
-        network: "dbcMainnet",
-        chainId: 19880818,
-        urls: {
-          apiURL: "https://blockscout.dbcscan.io/api",
-          browserURL: "https://blockscout.dbcscan.io",
-        },
-      }
     ]
   }
 };
